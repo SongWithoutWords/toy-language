851975f79ee1ad5d47e984b9835bdb61c0dde91e
ian <ianmccall93@gmail.com>
2017-09-23 23:11:43 -0700

    created the ast

---
+++ src/Ast.hs
@@ -0,0 +1,69 @@
+{-# language DataKinds #-}
+{-# language GADTs #-}
+{-# language StandaloneDeriving #-}
+
+module Ast where
+
+data Typing
+  = U -- untyped
+  | T -- typed
+
+type Name = String
+
+data Named a = Named Name a
+  deriving(Eq, Show)
+
+data Program t where
+  Program :: [Named (Expr t)] -> Expr t -> Program t
+
+deriving instance Eq(Program t)
+deriving instance Show(Program t)
+
+
+type ExprU = Expr 'U
+type ExprT = Expr 'T
+data Expr t where
+  ExprU :: ExprU' -> ExprU
+  ExprT :: Type -> ExprT' -> ExprT
+
+deriving instance Eq(Expr t)
+deriving instance Show(Expr t)
+
+
+type ExprU' = Expr' 'U
+type ExprT' = Expr' 'T
+data Expr' t where
+  EVar :: Name -> Expr' t
+
+  -- Add multiple parameters later, as needed
+  ELam :: Name -> Expr t -> Expr' t
+  EApp :: Expr t -> Expr t -> Expr' t
+
+  EIf :: Expr t -> Expr t -> Expr t -> Expr' t
+  EBinOp :: Op -> Expr t -> Expr t -> Expr' t
+  EVal :: Val -> Expr' t
+
+deriving instance Eq(Expr' t)
+deriving instance Show(Expr' t)
+
+
+data Op
+  = Add
+  | Sub
+  | Mul
+  | Div
+  | And
+  | Or
+  deriving(Eq, Show)
+
+data Val
+  = VBln Bool
+  | VInt Int
+  deriving(Eq, Show)
+
+data Type
+  = TLam Type Type
+  | TBln
+  | TInt
+  deriving(Eq, Show)
+
