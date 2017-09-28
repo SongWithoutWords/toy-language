77fc1aa9fa8988eda72add5cc662ca1f450f9a1b
ian <ianmccall93@gmail.com>
2017-09-27 23:18:44 -0700

    added mapConstraint to Constraint module

--- src/Constraint.hs
+++ src/Constraint.hs
@@ -8,3 +8,7 @@ import Type
 data Constraint
   = Constraint Type Type
   deriving(Eq, Show)
+
+mapConstraint :: (Type -> Type) -> Constraint -> Constraint
+mapConstraint f (Constraint t1 t2) = Constraint (f t1) (f t2)
+
