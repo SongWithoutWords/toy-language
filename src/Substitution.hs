ef32be112b5d2fdb411bdffbe302f748ad753d27
ian <ianmccall93@gmail.com>
2017-09-27 23:27:44 -0700

    added subType to substitution

--- src/Substitution.hs
+++ src/Substitution.hs
@@ -1,8 +1,17 @@
 module Substitution where
 
-import Data.Map
+import qualified Data.Map as M
 
 import Type
 
-type Substitutions = Map TypeVar Type
+type Substitutions = M.Map TypeVar Type
+
+subType :: Substitutions -> Type -> Type
+subType s (TLam t1 t2) = TLam (subType s t1) (subType s t2)
+subType _ TBln = TBln
+subType _ TInt = TInt
+subType _ TError = TError
+subType s (TVar tvar) = case M.lookup tvar s of
+  Nothing -> TVar tvar
+  Just t -> t
 
