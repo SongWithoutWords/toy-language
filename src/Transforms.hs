07e3b7f5b77874ca0c388ff190215e80d6701b22
ian <ianmccall93@gmail.com>
2017-09-27 23:49:35 -0700

    added transforms module to combine various smaller stages

---
+++ src/Transforms.hs
@@ -0,0 +1,16 @@
+module Transforms
+  ( module Transforms
+  , module Ast
+  ) where
+
+import Ast
+import ConstraintGen
+import SubAst
+import Unify
+
+typeCheckAst :: AstU -> AstT
+typeCheckAst ast =
+  let (ast', constraints) = constrainAst ast in
+  let substitutions = unifyConstraints constraints in
+  subAst substitutions ast'
+
