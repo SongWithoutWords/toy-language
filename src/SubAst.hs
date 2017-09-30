1b469674809c470638cfb956e57cbd9cca1f92b0
ian <ianmccall93@gmail.com>
2017-09-29 21:13:55 -0700

    fixed a bug in SubAst

--- src/SubAst.hs
+++ src/SubAst.hs
@@ -17,7 +17,7 @@ subExpr s (ExprT t e) = ExprT (subType s t) (subExpr' s e)
 subExpr' :: Substitutions -> ExprT' -> ExprT'
 subExpr' s expr = let sub = subExpr s in case expr of
   EVar n -> EVar n
-  ELamT nt e -> ELamT nt $ sub e
+  ELamT (Named n t) e -> ELamT (Named n $ subType s t) $ sub e
   EApp e1 e2 -> EApp (sub e1) (sub e2)
   EIf (Pred e1) e2 e3 -> EIf (Pred $ sub e1) (sub e2) (sub e3)
   EBinOp op e1 e2 -> EBinOp op (sub e1) (sub e2)
